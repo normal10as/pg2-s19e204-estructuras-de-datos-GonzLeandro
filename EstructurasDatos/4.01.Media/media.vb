adfffbcb9a63c659fca33d3b74baf8a023f07e21
GonzLeandro <joayugi@gmail.com>
2019-07-01 19:41:41 -0300

    v. 0.3

--- EstructurasDatos/4.01.Media/media.vb
+++ EstructurasDatos/4.01.Media/media.vb
@@ -3,22 +3,18 @@ Imports System
 Module media
     Dim array(4) As Integer
     Dim media As Integer
-
     Sub Main(args As String())
-        For x = 0 To array.Length - 1
-            Console.Write("Ingrese un valor: ")
-            calcular(x)
-        Next
-
+        calcular()
         Console.WriteLine("El contenido de la array es: ")
         For Each n In array
             Console.WriteLine("Elemento: " & n & " - (Elemento: " & n & " - Media: " & (media / array.Count) & " = " & n - (media / array.Count) & ")")
         Next
-
     End Sub
-
-    Sub calcular(x)
-        array(x) = Console.ReadLine
-        media += array(x)
+    Sub calcular()
+        For x = 0 To array.Length - 1
+            Console.Write("Ingrese un valor: ")
+            array(x) = Console.ReadLine
+            media += array(x)
+        Next
     End Sub
 End Module
