a5055b451422c2f78b53f1480b812b45f826f6ae
GonzLeandro <joayugi@gmail.com>
2019-07-01 19:31:54 -0300

    v. 0.1

---
+++ EstructurasDatos/4.01.Media/media.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module media
+    Dim array(4) As Integer
+    Dim media As Integer
+    Sub Main(args As String())
+        Console.WriteLine("Inicio")
+        calcular()
+        Console.WriteLine("El contenido de la array es: ")
+        For Each n In array
+            Console.WriteLine("Elemento: " & n & " - (Elemento: " & n & " - Media: " & (media / array.Count) & " = " & n - (media / array.Count) & ")")
+        Next
+    End Sub
+    Sub calcular()
+        For x = 0 To array.Length - 1
+            Console.Write("Ingrese un valor: ")
+            array(x) = Console.ReadLine
+            media += array(x)
+        Next
+    End Sub
+End Module
---
+++ EstructurasDatos/test/test.vb
@@ -0,0 +1,7 @@
+Imports System
+
+Module test
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
